83b5ce0a1e239a891fe724367ac8004ad18ec644
chacha86 <taejincha0619@gmail.com>
2024-05-23 01:03:20 +0900

    js-test-start

---
+++ src/main/java/com/example/jstest/JsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jstest/JsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
